61e5f606064081ecd3eacec58846b80add13ae77
Gian Sass <gian.sass@outlook.de>
2024-03-15 11:48:43 +0000

    add image augmentation methods

--- dataset.py
+++ dataset.py
@@ -7,6 +7,8 @@ import librosa
 
 from hparams import HParams
 
+AUGMENTATION_METHODS = 3
+
 class MerkelDataset(Dataset):
     def __init__(self, hparams: HParams):
         self.hparams = hparams
@@ -17,7 +19,7 @@ class MerkelDataset(Dataset):
                 self.filepaths.append(os.path.join(self.hparams.data_dir, f))
 
     def __len__(self):
-        return int(len(self.filepaths) * self.hparams.dataset_ratio) * self.hparams.dataset_batch_size 
+        return int(len(self.filepaths) * self.hparams.dataset_ratio) * self.hparams.dataset_batch_size * (AUGMENTATION_METHODS+1)
 
     def get_batch(self, file_idx):
         if file_idx in self.cached_batches:
@@ -33,15 +35,40 @@ class MerkelDataset(Dataset):
         return (X, Y)
 
     def __getitem__(self, idx):
-        file_idx = idx // self.hparams.dataset_batch_size 
-        data_idx = idx % self.hparams.dataset_batch_size 
+        norm_idx = idx // (AUGMENTATION_METHODS+1)
+        augm_idx = idx % (AUGMENTATION_METHODS+1)
+
+        file_idx = norm_idx // self.hparams.dataset_batch_size 
+        data_idx = norm_idx % self.hparams.dataset_batch_size 
 
         (X, Y) = self.get_batch(file_idx)
-
         x = X[data_idx]
         y = Y[data_idx]
 
+        x = self.perform_image_augmentation(x, augm_idx)
+
         return x, y
+
+    def perform_image_augmentation(self, x, augm):
+        if augm == 1:
+            # flip horizontally
+            x = x[:, :, :, ::-1]
+        elif augm == 2:
+            # Adjust saturation
+            # Convert image to grayscale (mean across the color channels, assuming x shape is C, T, H, W)
+            gray = np.mean(x, axis=0, keepdims=True)
+            # Interpolate between the grayscale image and the original image
+            alpha = 0.5  # Adjust this factor to increase or decrease saturation
+            x = alpha * x + (1 - alpha) * gray
+        elif augm == 3:
+            # Adjust contrast
+            # Calculate the mean pixel value across spatial dimensions
+            mean = np.mean(x, axis=(2, 3), keepdims=True)
+            alpha = 0.5  # Adjust this factor to increase or decrease contrast
+            x = alpha * (x - mean) + mean
+            # Clipping to maintain valid pixel range
+            x = np.clip(x, 0, 1)
+        return x
 
     def preload(self):
         len_batches = len(self)//self.hparams.dataset_batch_size
